4ab03804a351254998c648675fe73d2bdf2bdd3a
Mauro Ruiz <mcruizromero85@gmail.com>
2016-04-23 16:09:17 -0500

    Ajuste para poder subir al github

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function (grunt) {
       },
       heroku: {
         options: {
-            remote: 'git@heroku.com:satel-static',
+            remote: 'git@heroku.com:satel-static.git',
             branch: 'master'
         }
       }
