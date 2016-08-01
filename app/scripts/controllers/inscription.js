5b481544388419c4b285e069387e7a806287fddf
Mauro Ruiz <mcruizromero85@gmail.com>
2016-07-31 20:11:54 -0500

    Avance de inscripcion

--- app/scripts/controllers/inscription.js
+++ app/scripts/controllers/inscription.js
@@ -7,8 +7,7 @@
  * # MainCtrl
  * Controller of the ggesportsWebApp
  */
-angular.module('ggesportsWebApp')
-  .controller('InscriptionCtrl', function ($scope, $routeParams, $http) {
+angular.module('ggesportsWebApp').controller('InscriptionCtrl', function ($scope, $routeParams, $http) {
 
     var torneoId = $routeParams.torneoId;
 
@@ -34,17 +33,52 @@ angular.module('ggesportsWebApp')
         };
       
       console.log($scope.review);
-      $http({
-        method: 'POST',
-        url: 'http://localhost:3000/inscripciones',
-        data: data
-      })
-      .success(function(response) {
-        console.debug(response);
-      });
+      if ($scope.myForm.$valid){
+        $http({
+          method: 'POST',
+          url: 'http://localhost:3000/inscripciones',
+          data: data
+        })
+        .success(function(response) {
+          console.debug(response);
+        });
+      }
+      
 
   	}
 
-  	
 
-  });
+}).directive('myDirective', function() {
+      return {
+          require: 'ngModel',
+          link: function(scope, element, attr, mCtrl) {
+              function myValidation(value) {
+                  var pattern = /^\D.{2,11}#\d{4}$/;
+                  if (pattern.test(value)) {
+                      mCtrl.$setValidity('charE', true);
+                  } else {
+                      mCtrl.$setValidity('charE', false);
+                  }
+                  return value;
+              }
+              mCtrl.$parsers.push(myValidation);
+          }
+      };
+   })
+  .directive('myDirectiveEmail', function() {
+      return {
+          require: 'ngModel',
+          link: function(scope, element, attr, mCtrl) {
+              function myValidation(value) {
+                  var pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                  if (pattern.test(value)) {
+                      mCtrl.$setValidity('charE', true);
+                  } else {
+                      mCtrl.$setValidity('charE', false);
+                  }
+                  return value;
+              }
+              mCtrl.$parsers.push(myValidation);
+          }
+      };
+   });
